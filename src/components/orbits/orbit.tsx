b421836219f79c1808ccba0dc6d846315716fe30
laggu91 <laggu91@gmail.com>
2023-09-03 03:09:23 +0000

    feat: edit mode view
    Merge-request: ORBIT-MR-95
    Merged-by: 구원 라 <laggu91@gmail.com>

--- src/components/orbits/orbit.tsx
+++ src/components/orbits/orbit.tsx
@@ -14,7 +14,7 @@ interface ModeProps {
 
 function ReadMode({ orbit, setMode }: ModeProps) {
     return (
-        <div className="w-full">
+        <div className="flex flex-col gap-2">
             <div className="flex justify-between">
                 <div className="basis-10/12">
                     <div className="flex gap-6">
@@ -51,35 +51,51 @@ function ReadMode({ orbit, setMode }: ModeProps) {
                 </div>
             </div>
             <div>
-                <p>message</p>
+                <p className="text-lg font-semibold">message</p>
                 <div className="border rounded p-2">{orbit.message}</div>
             </div>
         </div>
     )
 }
 
-function WriteMode({ orbit, setMode }: ModeProps) {
+function EditMode({ orbit, setMode }: ModeProps) {
     return (
-        <div className="w-full">
-            <div className="flex flex-row">
-                <div className="basis-1/4">
-                    <p>channel name</p>
-                    <p>{orbit.channelName}</p>
+        <div className="flex flex-col gap-2">
+            <div className="flex justify-between">
+                <div className="basis-10/12">
+                    <form className="flex gap-6">
+                        <div className="w-40">
+                            <label htmlFor={`${orbit.id}/ChannelNameInput`} className="text-lg font-semibold">
+                                Channel Name
+                                <input id={`${orbit.id}/ChannelNameInput`} className="border rounded w-full p-1" value={orbit.channelName} />
+                            </label>
+                        </div>
+                        <div className="w-32">
+                            <label htmlFor={`${orbit.id}/FormatSelect`} className="text-lg font-semibold">
+                                Format
+                                <select id={`${orbit.id}/FormatSelect`} className="border rounded w-full p-1">
+                                    <option>cron</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div className="w-32">
+                            <label htmlFor={`${orbit.id}/TimezoneSelect`} className="text-lg font-semibold">
+                                timezone
+                                <select id={`${orbit.id}/TimezoneSelect`} className="border rounded w-full p-1">
+                                    <option>ETC/UTC</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div className="w-56">
+                            <label htmlFor={`${orbit.id}/CronInput`} className="text-lg font-semibold">
+                                cron
+                                <input id={`${orbit.id}/CronInput`} className="border rounded w-full p-1" value={orbit.cron} />
+                            </label>
+                        </div>
+                    </form>
                 </div>
-                <div className="basis-1/4">
-                    <p>format</p>
-                    <p>{orbit.format}</p>
-                </div>
-                <div className="basis-1/4">
-                    <p>timezone</p>
-                    <p>{orbit.timezone}</p>
-                </div>
-                <div className="basis-1/4">
-                    <p>cron</p>
-                    <p>{orbit.cron}</p>
-                </div>
-                <div className="basis-1/8">
-                    <div className="flex space-x-2">
+                <div className="basis-2/12">
+                    <div className="flex gap-2 justify-end">
                         <button type="button" onClick={() => setMode('read')} className="w-6 h-6">
                             <Stop />
                         </button>
@@ -90,8 +106,10 @@ function WriteMode({ orbit, setMode }: ModeProps) {
                 </div>
             </div>
             <div>
-                <p>message</p>
-                <p>{orbit.cron}</p>
+                <label htmlFor={`${orbit.id}/MessageTextarea`} className="flex flex-col text-lg font-semibold">
+                    message
+                    <textarea id={`${orbit.id}/MessageTextarea`} className="border rounded p-2" value={orbit.message} />
+                </label>
             </div>
         </div>
     )
@@ -101,7 +119,7 @@ export default function Orbit({ orbit }: OrbitProps) {
     const [mode, setMode] = useState<string>(orbit.mode)
     return (
         <div className="border-2 border-[#9C4A98] rounded p-2 w-full">
-            {mode === 'read' ? <ReadMode orbit={orbit} setMode={setMode} /> : <WriteMode orbit={orbit} setMode={setMode} />}
+            {mode === 'read' ? <ReadMode orbit={orbit} setMode={setMode} /> : <EditMode orbit={orbit} setMode={setMode} />}
         </div>
     )
 }
