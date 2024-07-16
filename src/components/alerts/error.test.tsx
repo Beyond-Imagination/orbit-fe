aaf7d6ad918ef3013b927db351c5932232e3ecf4
laggu91 <laggu91@gmail.com>
2024-07-16 04:36:28 +0000

    test: ErrorAlert component test
    Merge-request: ORBIT-MR-227
    Merged-by: 구원 나 <laggu91@gmail.com>

---
+++ src/components/alerts/error.test.tsx
@@ -0,0 +1,17 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { faker } from '@faker-js/faker'
+
+import ErrorAlert from './error'
+
+describe('ErrorAlert', () => {
+    it('renders a heading', () => {
+        const errorMessage = faker.lorem.slug()
+
+        render(<ErrorAlert message={errorMessage} />)
+
+        const error = screen.getByText(errorMessage)
+
+        expect(error).toBeInTheDocument()
+    })
+})
