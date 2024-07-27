bf5090586499afdfd72bfe904c5b822d54c83af8
laggu91 <laggu91@gmail.com>
2024-07-27 16:17:28 +0000

    test: orbit component test
    Merge-request: ORBIT-MR-232
    Merged-by: 구원 나 <laggu91@gmail.com>

---
+++ src/components/orbits/Oribt.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from '@testing-library/react'
+import { useState } from 'react'
+
+import Orbit from '@/components/orbits/orbit'
+import { IOrbit } from '@/types'
+
+jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useState: jest.fn(),
+}))
+
+jest.mock('@/components/orbits/orbitUpdate', () => () => <div>OrbitUpdate</div>)
+jest.mock('@/components/orbits/orbitRead', () => () => <div>OrbitRead</div>)
+
+describe('Orbit', () => {
+    const useStateMock = useState as jest.Mock
+    it('renders a Orbit on read mode', () => {
+        useStateMock.mockImplementation(() => [false, jest.fn()])
+
+        const orbit: IOrbit = {
+            _id: 'id',
+            type: 'type',
+            channelName: 'name',
+            timezone: 'timezone',
+            message: 'message',
+            status: 'status',
+        }
+
+        render(<Orbit orbit={orbit} />)
+
+        const orbitRead = screen.getByText('OrbitRead')
+
+        expect(orbitRead).toBeInTheDocument()
+    })
+
+    it('renders a Orbit on update mode', () => {
+        useStateMock.mockImplementation(() => [true, jest.fn()])
+
+        const orbit: IOrbit = {
+            _id: 'id',
+            type: 'type',
+            channelName: 'name',
+            timezone: 'timezone',
+            message: 'message',
+            status: 'status',
+        }
+
+        render(<Orbit orbit={orbit} />)
+
+        const orbitUpdate = screen.getByText('OrbitUpdate')
+
+        expect(orbitUpdate).toBeInTheDocument()
+    })
+})
